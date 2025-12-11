24d83ec1e55bf2666bd073fe2bab40aa79fee7db
artemuniversidade-rgb <artemuniversidade@gmail.com>
2025-12-11 16:48:41 +0000

    Adicionada rota raiz / para resolver erro do Vercel e corrigido URI do Atlas.

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const dbURI = 'mongodb+srv://dev_admin:TWD2025@cluster0.xetcpa1.mongodb.net/?appName=Cluster0';
+// ATENÇÃO: Adicionei o nome da BD (app_menu) ao seu URI, 
+// pois estava em falta, o que causaria erro de conexão.
+const dbURI = 'mongodb+srv://dev_admin:TWD2025@cluster0.xetcpa1.mongodb.net/app_menu?appName=Cluster0';
+
 
 app.use((req, res, next) => {
     const dataHora = new Date().toLocaleString('pt-PT');
@@ -15,6 +18,14 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 
+// ----------------------------------------------------
+// NOVO: Rota de HOMEPAGE (para o Vercel não dar "Cannot GET /")
+app.get('/', (req, res) => {
+    res.status(200).send('Servidor TDW do Restaurante está ativo e a funcionar! Use /menu para aceder à API.');
+});
+// ----------------------------------------------------
+
+
 mongoose.connect(dbURI)
     .then(() => {
         console.log('Ligação ao MongoDB Atlas estabelecida com sucesso.');
@@ -22,10 +33,12 @@ mongoose.connect(dbURI)
         const menuRouter = require('./Controllers/menu_do_dia');
         app.use('/menu', menuRouter);
 
+        // A linha app.listen() deve permanecer dentro do .then()
         app.listen(port, () => {
           console.log(`Servidor "O Manjar do Chef" a correr na porta ${port}.`);
         });
     })
     .catch((err) => {
         console.log(`Erro na ligação ao MongoDB Atlas: ${err}`);
+        // No Vercel, o erro aqui causava o 500 FUNCTION_INVOCATION_FAILED
     });
